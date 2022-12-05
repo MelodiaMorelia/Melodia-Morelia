02a6c9039f1f3fdb66984db653d8060d4193b2a0
Gaby <gabrielasotorz@gmail.com>
2022-12-05 07:28:01 -0600

    Tercer Commit

--- js/app.js
+++ js/app.js
@@ -1,3 +1,15 @@
+// Se obtiene url del servidor.
+var url=window.location.href;
+
+//Se define que el sw.js se encuentra
+//en el repositorio.
+var ubicacionSw='/Melodia-Morelia/sw.js';
+
 if ( navigator.serviceWorker ) {
-    navigator.serviceWorker.register('/sw.js');
+    if(url.includes('localhost')){
+        ubicacionSw='/sw.js';
+    }
+
+    navigator.serviceWorker.register(ubicacionSw);
+
 }
